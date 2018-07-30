81a5a393b88e8b000b932db235d7cad02f270145
Cameron Akhavan <camerona@cameron-akhavan.local>
2018-07-29 23:28:20 -0700

    pubnub stuff

--- Planty.py
+++ Planty.py
@@ -7,6 +7,9 @@ from time import sleep
 import pubnub
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.pubnub import PubNub
+from pubnub.callbacks import SubscribeCallback
+from pubnub.enums import PNOperationType, PNStatusCategory
+
  
 pnconfig = PNConfiguration()
 pnconfig.subscribe_key = "sub-c-a667485c-757f-11e8-9f59-fec9626a7085"
@@ -24,6 +27,62 @@ pin = 17
 
 #Soil Moisture sensor is connected to GPIO14 as a button
 soil = Button(14)
+
+
+
+class MySubscribeCallback(SubscribeCallback):
+    def status(self, pubnub, status):
+        pass
+        # The status object returned is always related to subscribe but could contain
+        # information about subscribe, heartbeat, or errors
+        # use the operationType to switch on different options
+        if status.operation == PNOperationType.PNSubscribeOperation \
+                or status.operation == PNOperationType.PNUnsubscribeOperation:
+            if status.category == PNStatusCategory.PNConnectedCategory:
+                pass
+                # This is expected for a subscribe, this means there is no error or issue whatsoever
+            elif status.category == PNStatusCategory.PNReconnectedCategory:
+                pass
+                # This usually occurs if subscribe temporarily fails but reconnects. This means
+                # there was an error but there is no longer any issue
+            elif status.category == PNStatusCategory.PNDisconnectedCategory:
+                pass
+                # This is the expected category for an unsubscribe. This means there
+                # was no error in unsubscribing from everything
+            elif status.category == PNStatusCategory.PNUnexpectedDisconnectCategory:
+                pass
+                # This is usually an issue with the internet connection, this is an error, handle
+                # appropriately retry will be called automatically
+            elif status.category == PNStatusCategory.PNAccessDeniedCategory:
+                pass
+                # This means that PAM does allow this client to subscribe to this
+                # channel and channel group configuration. This is another explicit error
+            else:
+                pass
+                # This is usually an issue with the internet connection, this is an error, handle appropriately
+                # retry will be called automatically
+        elif status.operation == PNOperationType.PNSubscribeOperation:
+            # Heartbeat operations can in fact have errors, so it is important to check first for an error.
+            # For more information on how to configure heartbeat notifications through the status
+            # PNObjectEventListener callback, consult <link to the PNCONFIGURATION heartbeart config>
+            if status.is_error():
+                pass
+                # There was an error with the heartbeat operation, handle here
+            else:
+                pass
+                # Heartbeat operation was successful
+        else:
+            pass
+            # Encountered unknown status type
+    
+    def message(self, pubnub, message):
+        print(message);
+ 
+ 
+pubnub.add_listener(MySubscribeCallback())
+pubnub.subscribe().channels('ch1').execute()
+
+
 
 def publish_callback(result, status):
     pass
@@ -67,3 +126,5 @@ while True:
 	    sleep(1)
 	else:
 	    pump.on()
+
+	sleep(1)

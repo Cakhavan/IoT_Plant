07eeaf090c19fb61af74036478739a8a97b9592a
Tyler Lugger <tlugger@n.io>
2018-10-30 09:35:23 -0600

    PEP8 the python client

--- Planty.py
+++ Planty.py
@@ -5,7 +5,7 @@ from time import sleep
 from os import getenv
 from os.path import join, dirname
 from dotenv import load_dotenv
- 
+
 import pubnub
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.pubnub import PubNub
@@ -17,12 +17,12 @@ dotenv_path = join(dirname(__file__), '.env')
 
 # Load file from the path.
 load_dotenv(dotenv_path)
- 
+
 pnconfig = PNConfiguration()
-pnconfig.subscribe_key = getenv('SUB_KEY') 
+pnconfig.subscribe_key = getenv('SUB_KEY')
 pnconfig.publish_key = getenv('PUB_KEY')
 pnconfig.ssl = False
- 
+
 pubnub = PubNub(pnconfig)
 
 # Pump is connected to GPIO as an LED
@@ -32,50 +32,63 @@ pump = LED(int(getenv('PUMP_PIN')))
 sensor = 22
 pin = int(getenv('TEMP_PIN'))
 
-#Soil Moisture sensor is connected to GPIO as a button
+# Soil Moisture sensor is connected to GPIO as a button
 soil = Button(int(getenv('SOIL_PIN')))
 
 # Flag to automatically irrigate based on sensor readings
+global auto
 auto = True
 
 # My pump is wired so a value of 1 or 'on' will turn it on
 pump.off()
 
+print("Status Initialized")
+
+
 class MySubscribeCallback(SubscribeCallback):
     def status(self, pubnub, status):
         pass
-        # The status object returned is always related to subscribe but could contain
-        # information about subscribe, heartbeat, or errors
+        # The status object returned is always related to subscribe but could
+        # contain information about subscribe, heartbeat, or errors
         # use the operationType to switch on different options
         if status.operation == PNOperationType.PNSubscribeOperation \
                 or status.operation == PNOperationType.PNUnsubscribeOperation:
             if status.category == PNStatusCategory.PNConnectedCategory:
                 pass
-                # This is expected for a subscribe, this means there is no error or issue whatsoever
+                # This is expected for a subscribe, this means there is no
+                # error or issue whatsoever
             elif status.category == PNStatusCategory.PNReconnectedCategory:
                 pass
-                # This usually occurs if subscribe temporarily fails but reconnects. This means
-                # there was an error but there is no longer any issue
+                # This usually occurs if subscribe temporarily fails but
+                # reconnects. This means there was an error but there is no
+                # longer any issue
             elif status.category == PNStatusCategory.PNDisconnectedCategory:
                 pass
-                # This is the expected category for an unsubscribe. This means there
-                # was no error in unsubscribing from everything
-            elif status.category == PNStatusCategory.PNUnexpectedDisconnectCategory:
+                # This is the expected category for an unsubscribe. This means
+                # there was no error in unsubscribing from everything
+            elif status.category == PNStatusCategory.\
+                    PNUnexpectedDisconnectCategory:
                 pass
-                # This is usually an issue with the internet connection, this is an error, handle
-                # appropriately retry will be called automatically
+                # This is usually an issue with the internet connection, this
+                # is an error, handle appropriately retry will be called
+                # automatically
             elif status.category == PNStatusCategory.PNAccessDeniedCategory:
                 pass
-                # This means that PAM does allow this client to subscribe to this
-                # channel and channel group configuration. This is another explicit error
+                # This means that PAM does allow this client to subscribe to
+                # this channel and channel group configuration. This is another
+                # explicit error
             else:
                 pass
-                # This is usually an issue with the internet connection, this is an error, handle appropriately
+                # This is usually an issue with the internet connection, this
+                # is an error, handle appropriately
                 # retry will be called automatically
         elif status.operation == PNOperationType.PNSubscribeOperation:
-            # Heartbeat operations can in fact have errors, so it is important to check first for an error.
-            # For more information on how to configure heartbeat notifications through the status
-            # PNObjectEventListener callback, consult <link to the PNCONFIGURATION heartbeart config>
+            # Heartbeat operations can in fact have errors, so it is important
+            # to check first for an error.
+            # For more information on how to configure heartbeat notifications
+            # through the status
+            # PNObjectEventListener callback, consult <link to the
+            # PNCONFIGURATION heartbeart config>
             if status.is_error():
                 pass
                 # There was an error with the heartbeat operation, handle here
@@ -85,60 +98,68 @@ class MySubscribeCallback(SubscribeCallback):
         else:
             pass
             # Encountered unknown status type
- 
+
     def presence(self, pubnub, presence):
         pass  # handle incoming presence data
- 
+
     def message(self, pubnub, message):
+        print("Handling Message")
         if message.message == 'ON':
-            global auto
             auto = True
         elif message.message == 'OFF':
-            global auto
             auto = False
         elif message.message == 'WATER':
             # Manually trigger watering
             pump.on()
             sleep(5)
             pump.off()
- 
- 
+
 pubnub.add_listener(MySubscribeCallback())
 pubnub.subscribe().channels('ch1').execute()
 
+
 def publish_callback(result, status):
-	pass
+    pass
+
 
 def soil_is_dry():
-	if soil.is_held:
+    print("Checking Soil")
+    if soil.is_held:
         # Moisture sensor will return 1 when wet
-		return False
-	else:
-		return True
+        return False
+    else:
+        return True
 
 while True:
-	if auto:
-		# Try to grab a sensor reading.  Use the read_retry method which will retry up
-		# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-		humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-		DHT_Read = ('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-		print(DHT_Read)
+        if auto:
+            # Try to grab a sensor reading.  Use the read_retry method which
+            # will retry up to 15 times to get a sensor reading (waiting 2
+            # seconds between each retry).
+            humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+            DHT_Read = ('Temp={0:0.1f}*  Humidity={1:0.1f}%'.
+                        format(temperature, humidity))
+            print(DHT_Read)
 
-		dictionary = {"eon": {"Temperature": temperature, "Humidity": humidity}}
-		pubnub.publish().channel('ch2').message([DHT_Read]).async(publish_callback)
-		pubnub.publish().channel("eon-chart").message(dictionary).async(publish_callback)
-		
-		if soil_is_dry():
-		    print("turning on")
-		    pump.on()
-		    sleep(5)
-		    print("pump turning off")
-		    pump.off()
-		    sleep(1)
-		else:
-		    pump.off()
+            dictionary = {"eon": {"Temperature": temperature,
+                                  "Humidity": humidity}}
+            pubnub.publish().channel('ch2').message([DHT_Read]).\
+                meta({'test': 'meta'}).async(publish_callback)
+            pubnub.publish().channel("eon-chart").message(dictionary).\
+                meta({'test': 'meta'}).async(publish_callback)
 
-		sleep(1)
-	else:
-        pump.off()
-        sleep(3)
+            if soil_is_dry():
+                print("Turning pump on")
+                pump.on()
+                sleep(5)
+                print("Turning pump off")
+                pump.off()
+                sleep(1)
+            else:
+                print("Soil is good")
+                pump.off()
+
+            sleep(1)
+        else:
+            print("In Manual Mode")
+            pump.off()
+            sleep(3)
